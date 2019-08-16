5a0a845e7c33f588175cd2c3b8fd874db0c3a570
O.M <a.majstrenko@everad.com>
2019-08-16 18:31:53 +0300

    upload project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// $(function () {
+//   var wall = new Freewall("#consumers");
+//   wall.reset({
+//     selector: '.consumers__item',
+//     animate: true,
+//     cellW: 20,
+//     cellH: 200,
+//     onResize: function () {
+//       wall.fitWidth();
+//     }
+//   });
+//   wall.fitWidth();
+//   // for scroll bar appear;
+//   $(window).trigger("resize");
+// });
+// $(function () {
+//   $('#consumers').masonry({
+//     itemSelector: '.consumers__item',
+//     columnWidth: 50,
+//     gutter: 10,
+//     fitWidth: true
+//   });
+// });
